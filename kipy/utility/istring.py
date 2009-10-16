e55bf641347012647b54ee682f4473dfd73fa9e1
pmaupin <pmaupin@a2a4f1e4-b939-11de-bda7-396a5af1c03d>
2009-10-16 18:50:55 +0000

    Made new IndexedString class standalone
    git-svn-id: http://pyeda.googlecode.com/svn/trunk/kipy@8 a2a4f1e4-b939-11de-bda7-396a5af1c03d

--- kipy/utility/istring.py
+++ kipy/utility/istring.py
@@ -1,24 +1,18 @@
 import re
-
-indexedsplitter = re.compile('([0-9]+)').split
-
-def splitstr(s):
-    for item in indexedsplitter(s):
-        if not item:
-            continue
-        elif item.isdigit():
-            yield int(item)
-        else:
-            yield item
+import types
 
 class IndexedString(str):
     """   An indexed string is designed to sort properly alphanumerically
     """
+    _indexedsplitter = types.MethodType(re.compile('([0-9]+)').split, None, str)
+
     def __init__(self, *whatever):
         # Put the string itself into the key so that
         # 'xxx00' doesn't equal 'xxx0'
-        self._key = tuple(splitstr(self)) + (str(self),)
-        self._hash = hash(self._key)
+        key = [int(x) if x.isdigit() else x for x in self._indexedsplitter()]
+        key.append(str(self))
+        self._key = key = tuple(key)
+        self._hash = hash(key)
 
     def __hash__(self):
         return self._hash
