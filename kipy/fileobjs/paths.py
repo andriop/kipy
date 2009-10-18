fc7fa04b7108954142e9b11ff2f5718c13ab63d2
pmaupin <pmaupin@a2a4f1e4-b939-11de-bda7-396a5af1c03d>
2009-10-18 19:30:29 +0000

    Updated paths to support /usr/local
    git-svn-id: http://pyeda.googlecode.com/svn/trunk/kipy@12 a2a4f1e4-b939-11de-bda7-396a5af1c03d

--- kipy/fileobjs/paths.py
+++ kipy/fileobjs/paths.py
@@ -1,9 +1,31 @@
-from os.path import join
+from ..utility import FileAccess
 
-# Put some Linux/MS selection stuff here
-kicad_root = '/usr/share/kicad'
-kicad_demo_root = '/usr/share/doc/kicad/demos'
+'''
+Locations of KiCad stuff, by system.
+
+Unfortunately, there seems to be different places to store things,
+depending on distribution.
+
+MS Windows locations deferred -- I don't have a Windows system.
+
+But I do have two different linux boxes with different installs.
+
+Don't know if this is the best way to do things, but here it is.
+
+'''
 
 
-kicad_lib_root = join(kicad_root, 'library')
-kicad_default_project = join(kicad_root, 'template/kicad.pro')
+kicad_root = FileAccess('/usr/local/kicad/share')
+if kicad_root.exists:
+    kicad_demo_root = kicad_root.demos
+else:
+    kicad_root = FileAccess('/usr/share/kicad')
+    kicad_demo_root = kicad_root[-1].doc.kicad.demos
+
+kicad_lib_root = kicad_root.library
+kicad_default_project = kicad_root.template.kicad + '.pro'
+
+assert kicad_root.exists, 'Could not find Kicad root directory %s' % kicad_root
+assert kicad_lib_root.exists, 'Could not find Kicad library directory %s' % kicad_lib_root
+assert kicad_demo_root.exists, 'Could not find Kicad demo directory %s' % kicad_demo_root
+assert kicad_default_project.exists,  'Could not find default project %s' % kicad_default_project
