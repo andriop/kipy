33c9e4b7841d3b93e59c4fe82228a943c8ad16dd
pmaupin <pmaupin@a2a4f1e4-b939-11de-bda7-396a5af1c03d>
2009-10-19 20:16:37 +0000

    Dump full BOM in tab-delimited spreadsheet
    git-svn-id: http://pyeda.googlecode.com/svn/trunk/kipy@13 a2a4f1e4-b939-11de-bda7-396a5af1c03d

--- tools/makebom.py
+++ tools/makebom.py
@@ -59,7 +59,7 @@ def buildparts(sch):
                 base = other.fields.copy()
                 base.update(self.fields)
                 self.fields.update(base)
-            if self.footprint is not None:
+            if self.footprint in set(['0805']):
                 parttype = self.parttype, self.value, self.footprint
             else:
                 parttype = self.parttype, self.value
@@ -76,6 +76,7 @@ def buildparts(sch):
 def checkparts(by_part_type):
     # Make sure that for each part type, there is only
     # one value for each field entry.
+
     ok = True
     result = []
     for partid, partset in sorted(by_part_type.iteritems()):
@@ -100,26 +101,102 @@ def checkparts(by_part_type):
 
 def getbom(parts):
     # This is my schematic-specific BOM info getter
+
+    result = []
+
     class BomEntry(object):
-        mouser = None
-        digikey = None
-        Info = None
+        disty = ''
+        info = ''
+        footprint = ''
+        mfg = ''
+        onhand = 0
+
+        def fixUVBDIN(self):
+            self.info = '96 pin male DIN'
+            del self.value
+
+        def fixRES(self):
+            self.info = 'RES %s ohm 5%% 0603' % self.value
+
+        def fixC(self):
+            if not self.info:
+                self.info = 'CAP CER %suF 25V 0603 X7R' % self.value
+
+        def fixLEDX(self):
+            self.info = 'THT Red LED T 1.75'
+
+        def fixSW_PUSH_5PIN(self):
+            self.info = '5 pin THT pushbutton'
+
+        def fixFILTER(self):
+            self.info = '0805 1K Ferrite Bead'
+
+        def fixAMP_POWER(self):
+            self.info = '10 pin red THT power conn'
+
+        def fixJPDRB(self):
+            refdes, = self.refdes
+            self.refdes = [refdes + 'A']
+            self.value = self.parttype = 'JP1X4'
+            result.append(BomEntry(self.__dict__))
+            self.refdes = [refdes + 'B']
+            self.value = self.parttype = 'JP1X3'
+            result.append(BomEntry(self.__dict__))
+            self.refdes = [refdes + 'C']
+            self.value = self.parttype = 'JP1X2'
+            getattr(self, 'fix' + self.parttype, self.generic)()
+
+        def generic(self):
+            if not self.info:
+                s = (self.mfg,
+                        self.value if self.value not in self.mfg else '',
+                        self.footprint)
+                self.info = ' '.join(x for x in s if x)
+            elif self.info and self.footprint and self.footprint not in self.info:
+                self.info = '%s %s' % (self.footprint, self.info)
 
         def __init__(self, info):
-            self.__dict__.update(info)
-            self.numparts = len(self.refdes)
+            self.__dict__.update((x.lower(), y) for (x,y) in info.iteritems())
+            getattr(self, 'fix' + self.parttype, self.generic)()
+            self.numparts = (0, len(self.refdes))[self.footprint != 'DNS']
             self.refdes = refdeslist(self.refdes)
 
-        def __cmp__(self, other):
-            return cmp(str(self), str(other))
-        def __str__(self):
-            return str(sorted(x for x in self.__dict__.iteritems()))
+        def sortorder(self):
+            sortdict = dict(refdes = '0')
+            items = (x for x in self.__dict__.iteritems())
+            return sorted(items, key = lambda x: sortdict.get(x[0], x[0]))
 
-    return sorted(BomEntry(x) for x in parts)
+        def __cmp__(self, other):
+            return cmp(self.sortorder(), other.sortorder())
+
+        def __str__(self):
+            return str(self.sortorder())
+
+    for x in parts:
+        result.append(BomEntry(x))
+    resdict = {}
+    for x in result:
+        resdict.setdefault(x.info, []).append(x)
+    for x in resdict.itervalues():
+        if len(x) > 1:
+            x[0].refdes = ', '.join(y.refdes for y in x)
+            x[0].numparts = sum(y.numparts for y in x)
+            for y in x[1:]:
+                #print "merging %s" % y.info
+                result.remove(y)
+    result.sort()
+    return result
 
 partinfo = buildparts(sch)
 partinfo = checkparts(partinfo)
 partinfo = getbom(partinfo)
 
+print '\t'.join('Qty ** Description Reference BuildSize Extended Required Margin'.split()).replace('**', '  ')
+fmt = '\t'.join('%d ** "%s"         "%s"      %d        %d       %d       %d'.split()).replace('**', '  ')
+qty = 12
 for x in partinfo:
-    print x
+    onhand = int(x.onhand)
+    numparts = x.numparts
+    total = numparts * qty
+    print fmt % (numparts, x.info, x.refdes, qty,
+        total, max(0, total - onhand), max(0, onhand - total))
